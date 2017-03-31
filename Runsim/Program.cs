983329804e4279fd68546e9e92bbc86426b3f5a8
Nirun Tulayote <nirunnu1@hotmail.com>
2017-03-31 15:29:36 +0700

    add

---
+++ Runsim/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Simulations.Models;
+using Simulations;
+
+namespace Runsim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("add >>  ");
+            myDbContext context = new myDbContext();
+            List<testProfile> list = new List<testProfile>()
+            {
+                new testProfile(){ Uid = 1,Name = "AAA", Case1= 5,Case2= 10, Case3= 15 },
+                new testProfile(){ Uid = 2,Name = "BBB", Case1= 15,Case2= 10, Case3= 5 },
+                new testProfile(){ Uid = 3,Name = "CCC", Case1= 5,Case2= 15, Case3= 10 }
+            };
+            context.testProfile.AddRange(list);
+            context.SaveChanges();
+            List<SettingCase> listcase = new List<SettingCase>()
+            {
+                new SettingCase(){Uid = new Guid(),Name = "DefaultIf",Case1 = 33,Case2=33,Case3=35 }
+            };
+            context.SettingCase.AddRange(listcase);
+            context.SaveChanges();
+        }
+        
+    }
+}
